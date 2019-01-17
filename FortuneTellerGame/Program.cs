37f7990e1a12a3d357e123dbb2ccca1d28075c19
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-16 20:46:16 -0500

    First Commit

---
+++ FortuneTellerGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FortuneTellerGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            Console.ReadLine();
+            int userInput;
+            userInput = Console.Read();
+            Console.WriteLine("What is your birth month?", userInput);
+            string ROYGBIV = Console.ReadLine();
+            Console.WriteLine("Enter the word help to get a list of ROYGBIV");
+            Console.ReadLine();
+            Console.WriteLine(" The ROYGBIV colors are red, orange, yellow, green, blue, indigo, violet.");
+            Console.WriteLine("Which is your favorite Color?");
+           if (ROYGBIV =
+            {
+              
+            }
+            
+
+                
+        }
+    }
+}
