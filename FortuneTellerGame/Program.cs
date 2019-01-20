85eabfe800a86fc4a926db7da7bf1eb9409ed872
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-19 23:55:18 -0500

    Switch Statement Not working

--- FortuneTellerGame/Program.cs
+++ FortuneTellerGame/Program.cs
@@ -31,28 +31,27 @@ namespace FortuneTellerGame
 
             Console.WriteLine("What is the number of your birth month?");
             int birthMonth = Convert.ToInt32(Console.ReadLine());
-
-            
+            string moneyInBank;            
 
             //if statement for birth month
 
             if (birthMonth >= 0 && birthMonth <= 5)
             {
-                Console.WriteLine("$256,000.76");
+                moneyInBank = "$256,000.76";
             }
             else if (birthMonth >= 5 && birthMonth <= 9)
 
             {
-                Console.WriteLine("$3,687,105.42");
+                moneyInBank = "$3,687,105.42";
             }
             else if (birthMonth >= 9 && birthMonth <= 13)
 
             {
-                Console.WriteLine("$86.23");
+                moneyInBank = "$86.23";
             }
             else
             {
-                Console.WriteLine("0.00");
+                moneyInBank = "0.00";
             }
 
             Console.WriteLine("Which is your favorite Color?");
@@ -66,69 +65,69 @@ namespace FortuneTellerGame
                 roygbiv = Console.ReadLine();
             }
 
-            string modeOfTransportation = roygbiv;
+            string modeOfTransportation;
 
 
-            ///Do switch statement for colors to cars  colors should be in quotes
+            ///Do switch statement for colors to cars,  cars should be in quotes
 
             switch (roygbiv)
             {
                 case "red":
-                    Console.WriteLine("48 inch deck Kubota");
+                    modeOfTransportation = "48 inch deck Kubota";
                     break;
                 case "orange":
-                    Console.WriteLine("Amphibus");
+                    modeOfTransportation = "Amphibus";
                     break;
                 case "yellow":
-                    Console.WriteLine("treadmill bike");
+                    modeOfTransportation = "treadmill bike";
                     break;
                 case "green":
-                    Console.WriteLine("forklift");
+                    modeOfTransportation = "forklift";
                     break;
                 case "blue":
-                    Console.WriteLine("jetpack");
+                    modeOfTransportation = "jetpack";
                     break;
                 case "indigo":
-                    Console.WriteLine("hovercraft");
+                    modeOfTransportation = "hovercraft";
                     break;
                 case "violet":
-                    Console.WriteLine("dog sled");
+                    modeOfTransportation = "dog sled";
                     break;
             }
 
             Console.WriteLine("How many siblings do you have?");
             int numberOfSiblings = Convert.ToInt32(Console.ReadLine());
 
-            int vacationHome = numberOfSiblings;
+            string vacationHome;
 
             //switch statement or if statement for town
 
             if (numberOfSiblings.Equals("0"))
             {
-                Console.WriteLine("China");
+                vacationHome = "China";
             }
             else if (numberOfSiblings.Equals("1"))
             {
-                Console.WriteLine("Ashtabula, Ohio");
+                vacationHome = "Ashtabula, Ohio";
             }
             else if (numberOfSiblings.Equals("2"))
             {
-                Console.WriteLine("Chicken Alaska");
+                vacationHome = "Chicken, Alaska";
             }
             else if (numberOfSiblings.Equals("3"))
             {
-                Console.WriteLine("Big Arm, Montana");
+                vacationHome = "Big Arm, Montana";
             }
             else if (numberOfSiblings.Equals("4"))
             {
-                Console.WriteLine("Bugtussle, Kentucky");
+                vacationHome = "Bugtussle, Kentucky";
             }
             else
             {
-                Console.WriteLine("Burnt Porcupine, Maine");
+                vacationHome = "Burnt Porcupine, Maine";
             }
 
-            string str = firstName + lastName + " will retire in" + retirement + " years with" + birthMonth + " in the bank, " + " a vacation home in" + vacationHome + " and travel by" + roygbiv + ".";
+            string str = firstName + lastName + " will retire in " + retirement + " years with " + moneyInBank + " in the bank, " + " a vacation home in " + vacationHome + " and travel by" + modeOfTransportation + ".";
 
             Console.WriteLine(str);
 
